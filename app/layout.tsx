a3145ad4a3ed2fd3b25bc3cf9a8af202e05d49c3
Fedir Melnyk <pluserwork24@gmail.com>
2024-04-13 10:47:02 +0300

    add base setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "../components/ThemeProvider";
 import "./globals.scss";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <ThemeProvider>
+        <body>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
